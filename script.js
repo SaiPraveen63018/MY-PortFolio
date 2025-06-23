310055999324640987a85db1d2d6f85171009c64
saipraveen63018 <saipraveen@gmail.com>
2025-06-23 11:24:19 +0530

    new files created

---
+++ script.js
@@ -0,0 +1,12 @@
+const typed = new Typed("#typed-text", {
+  strings: [
+    "I'm <strong style='color:#27ae60;'>Your Name</strong>",
+    "DevOps Engineer",
+    "AWS | Docker | Kubernetes | Jenkins",
+    "CI/CD Enthusiast | Automation Lover"
+  ],
+  typeSpeed: 55,
+  backSpeed: 25,
+  loop: true,
+  showCursor: true
+});
